0d39dd2228fea8ce7cc8e8515e7a43202ef1f993
Clancinio <dean.clancy91@gmail.com>
2021-11-03 12:52:01 +0000

    Displaying list of books

---
+++ src/app/book/book.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { BookService } from '../book.service';
+import { Book } from '../book';
+
+@Component({
+  selector: 'app-book',
+  templateUrl: './book.component.html',
+  styleUrls: ['./book.component.css'],
+})
+export class BookComponent implements OnInit {
+
+  books!: Book[];
+
+  constructor(private bookService : BookService) { }
+
+ ngOnInit(): void {
+    this.bookService.getBooks().subscribe({
+      next: (value: Book[]) => this.books = value,
+      complete: () => console.log('book service finished'),
+      error: (message) => message = message
+    })}
+}
